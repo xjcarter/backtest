f3f8bd753cd5a57bf44250b58068c6cdea0f9e04
James Carter <xjcarter@gmail.com>
2024-03-04 16:21:10 -0500

    update backtest

--- backtest.py
+++ backtest.py
@@ -1,3 +1,8 @@
+import pandas
+from datetime import date, datetime
+from prettytable import PrettyTable
+from indicators import StDev, MondayAnchor
+import calendar_calcs
 
 class DumpFormat(str, Enum):
     STDOUT = 'STDOUT'
@@ -15,19 +20,21 @@ class BackTest():
         json_config = {
                         "settings": 
                             {
-                                "ms20": 20,
-                                "indicator1": 200,
+                                "StDev": 50,
+                                "ma200": 200,
                                 "duration": 10
                             },
                         "wallet":
                             {
                                 "cash": 10000,
                                 "wallet_alloc_pct": 0.5,
+                                "borrow_margin_pct": 0.5
                             },
                         "trading_limits":
                             {
                                 "dollar_limit": 100000000,
-                                "position_limit: 100000
+                                "position_limit: 100000,
+                                "leverage_target": 1
                             }
         }
         """
@@ -40,6 +47,7 @@ class BackTest():
 
         self.wallet = 0 
         self.wallet_alloc_pct = 1
+        self.borrow_margin_pct = 1
         self._initialize_wallet()
 
         self.dollar_base = None
@@ -58,12 +66,19 @@ class BackTest():
         self.trades = list()
         self.trade_series = list()
 
+        ## indicators and tools
+
+        self.stdev = StDev(sample_size=50)
+        self.anchor = MondayAnchor(derived_len=20)
+        self.holidays = calendar_calcs.load_holidays()
+
 
     def _initialize_wallet(self):
         wallet_dict = self.config.get('wallet')
         if wallet_dict:
             self.wallet = wallet_dict.get('cash', 10000)
             self.wallet_alloc_pct = wallet_dict.get('wallet_alloc_pct', 1)
+            self.borrow_margin_pct = wallet_dict.get('borrow_margin_pct', 1)
 
     def _initialize_limits(self):
         limit_dict = self.config.get('trading_limits')
@@ -73,26 +88,39 @@ class BackTest():
 
     ## trade execution functions
 
-    def entry_OPEN(self):
+    def BUY(self, price, security):
+        wallet = self.config.get('wallet')
+        if wallet is None: 
+            return 0
+
+        cash = wallet.get('cash', 0)
+        alloc_pct = wallet('wallet_alloc_prc', 1)
+        borrow_margin_pct = wallet('borrow_margin_pct', 1)
+    
+
+
         pass
 
-    def entry_CLOSE(self):
+    def SELL(self, price, security):
         pass
 
-    def exit_OPEN(self):
+    def entry_OPEN(self, cur_dt, bar, ref_bar=None):
+        end_of_week = calendar_calcs.is_end_of_week(cur_dt, self.holidays)
+
+        if self.anchor.count() > 0:
+            anchor_bar, bkout = self.anchor.valueAt(0)
+            if bkout < 0 and end_of_week == False:
+                self.BUY( bar['Open'), self.security )
+
+    def entry_CLOSE(self, cur_dt, bar, ref_bar=None):
         pass
 
-    def exit_CLOSE(self):
+    def exit_OPEN(self, cur_dt, bar, ref_bar=None):
         pass
 
-    def check_stop(self):
+    def exit_CLOSE(self, cur_dt, bar, ref_bar=None):
         pass
 
-    def check_entry(self):
-        pass
-
-    def check_exit(self):
-        pass
 
     def calc_price_stop(self):
         ## calc stop based on price distribution
@@ -130,7 +158,7 @@ class BackTest():
         ## sharpe, etc.
         pass
 
-    def dump_trades(self, format=DumpFormat.STDOUT):
+    def dump_trades(self, format=DumpFormat.CSV):
         ## stdout, csv, html
         pass
     def dump_trades_series(self, format=DumpFormat.STDOUT):
@@ -141,7 +169,51 @@ class BackTest():
         pass
 
    
-    def run(self):
-        ## run the backtest
-        pass
+    def run(self, start_from_dt):
+
+        start_dt = None
+        if start_from_dt is not None:
+            start_dt = datetime.strptime(start_from_dt,"%Y-%m-%d").date()
+
+        holidays = calendar_calcs.load_holidays()
+
+        # i = integer index
+        # str_cur_dt= bar date string
+        # cur_dt = bar datetime = datetime.strptime(dt)
+        # bar = OHLC, etc data.
+        for i, str_cur_dt, cur_dt, bar in self.security.next_bar():
+
+            ref_bar = None
+            if self.ref_index is not None:
+                ref_bar = self.ref_index.fetch_bar(str_cur_dt)
+            
+            self.exit_OPEN(cur_dt, bar, ref_bar)
+            self.entry_OPEN(cur_dt, bar, ref_bar)
+
+            ## do analytics here
+
+            self.anchor.push((cur_dt, bar))
+            self.stdev.push(bar['Close'])
+
+            # collect all analytics, but don't start trading until we
+            # hit the the start_from_dt trading date
+            if start_dt is not None and cur_dt < start_dt: continue
+
+            self.exit_CLOSE(cur_dt, bar, ref_bar)
+            self entry_CLOSE(cur_dt, bar, ref_bar)
+
+            ## record trade info for thed day.
+
+            if self.entry.get('position',0) == 0:
+                self.update_position_limit()
+                self.update_dollar_alloc()
+                self.update_dollar_limit()
+
+        self.generate_metrics()
+
+        self.dump_trades()
+        f or fmt in [DumpFormat.STDOUT, DumpFormat.CSV]:
+            self.trade_series(fmt)
+        self.dump_metrics()
+
 
